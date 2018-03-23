33b475c8dffa7803edb02287662236fd79bf90ab
Thomas St.Pierre <thomas@samoht.ca>
2018-03-22 23:25:40 -0400

    Update sources on path change

--- src/app/modal-path-selector/modal-path-selector.component.ts
+++ src/app/modal-path-selector/modal-path-selector.component.ts
@@ -24,17 +24,34 @@ export class ModalPathSelectorComponent implements OnInit {
     //populate available choices
     this.availablePaths = this.SignalKService.getPathsByType(this.question.pathType).sort();
 
+
+
+    //populate sources for this path (or just the current setting if we know nothing about the path)
     let pathKey = this.question.key + 'Path';
     let sourceKey = this.question.key + 'Source';
-
     let pathObject = this.SignalKService.getPathObject(this.question.formGroup.controls[pathKey].value);
     if (pathObject === null) { 
       this.availableSources = [this.question.formGroup.controls[sourceKey].value]; 
     } else {
       this.availableSources = ['default'].concat(Object.keys(pathObject.sources));
     }
+
+    this.detectNewPath();
   }
 
+  detectNewPath() {
+    let pathKey = this.question.key + 'Path';
+    let sourceKey = this.question.key + 'Source';
+    this.question.formGroup.controls[pathKey].valueChanges.subscribe(newValue => {
+      let pathObject = this.SignalKService.getPathObject(newValue);
+      if (pathObject === null) { 
+        this.availableSources = ['default']; 
+      } else {
+        this.availableSources = ['default'].concat(Object.keys(pathObject.sources));
+      }
+      this.question.formGroup.controls[sourceKey].setValue('default');
+    });
+  }
 
   settingsDataUpdatePath() { // called when we choose a new path. resets the rest with default info of this path
     let pathKey = this.question.key + 'Path';
