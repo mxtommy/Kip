1b9436397d08dc31e8d78ff8a4aec1eaf200a530
David Godin <david.godin@avanade.com>
2020-05-15 22:54:14 -0400

    sound refactoring and general integration

--- src/app/alarm-menu/alarm-menu.component.ts
+++ src/app/alarm-menu/alarm-menu.component.ts
@@ -4,6 +4,13 @@ import { AppSettingsService, INotificationConfig } from '../app-settings.service
 import { Subscription } from 'rxjs';
 import { Howl } from 'howler';
 
+const alarmTrack = {
+  1000 : 'notification', //filler
+  1001 : 'alert',
+  1002 : 'warn',
+  1003 : 'alarm',
+  1004 : 'emergency',
+};
 
 @Component({
   selector: 'app-alarm-menu',
@@ -16,16 +23,17 @@ export class AlarmMenuComponent implements OnInit, OnDestroy {
   private notificationServiceSettings: Subscription;
 
   alarms: { [path: string]: Alarm };
-  rawAlarms: { [path: string]: Alarm };
+  alarmsStream: { [path: string]: Alarm };
+
+  ignoredPaths: string[] = [];
+
   alarmCount: number = 0;
   unAckAlarms: number = 0;
   blinkWarn: boolean = false;
   blinkCrit: boolean = false;
+  howlPlayer: Howl;
+  activeAlarmSoundtrack: number;
 
-  ignoredPaths: string[] = [];
-
-  warningSound;
-  critSound;
   notificationConfig: INotificationConfig;
 
   constructor(
@@ -38,51 +46,41 @@ export class AlarmMenuComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit() {
-
-    this.warningSound = new Howl({
-      src: ['assets/alarm-warn.mp3'],
-      loop: true,
-    });
-    this.critSound = new Howl({
-      src: ['assets/alarm-crit.mp3'],
-      loop: true,
-    });
-    // Alarm code
-
+    // init Alarm stream
     this.alarmSub = this.notificationsService.getAlarms().subscribe(
       message => {
-        this.rawAlarms = message;
+        this.alarmsStream = message;
         this.updateAlarms();
       }
     );
+    // init alarm player
+    this.howlPlayer = this.getPlayer(1000);
   }
 
   updateAlarms() {
     // we use this as a staging area to limit menu update events when we play to the Alarms record
     if (!this.notificationConfig.devices.showNormalState) {
-      for (const [path, alarm] of Object.entries(this.rawAlarms)) {
-        let alarm = this.rawAlarms[path];
+      for (const [path, alarm] of Object.entries(this.alarmsStream)) {
+        let alarm = this.alarmsStream[path];
 
-        if (alarm.notification['state'] == 'normal') {
-          delete this.rawAlarms[path];
+        if (alarm.notification['state'] == 'normal' && alarm.notification['type'] == 'device') {
+          delete this.alarmsStream[path];
           break;
         }
       }
     }
-    this.alarms = this.rawAlarms;
+    this.alarms = this.alarmsStream;
     this.updateMenu();
   }
 
   /**
-   * main alert management function. Called on Observable message
+   * main menu management function.
    */
   updateMenu() {
     this.alarmCount = Object.keys(this.alarms).length;
     this.unAckAlarms = 0;
     this.blinkWarn = false;
     this.blinkCrit = false;
-    this.warningSound.stop();
-    this.critSound.stop();
 
     if (this.alarmCount > 0) {
       // find worse alarm state
@@ -98,35 +96,42 @@ export class AlarmMenuComponent implements OnInit, OnDestroy {
         let vSev = 0;
 
         switch (alarm.notification['state']) {
-          case 'alert':
-          case 'warn':
+          //case 'nominal':       // not sure yet... spec not clear. Maybe only relevant for Zones
+          case 'normal':        // information only ie.: engine temperature normal. Not usually displayed
+            if (alarm.notification['method'].includes('sound')) { aSev = 0; }
+            if (alarm.notification['method'].includes('visual')) { aSev = 0; }
+            break;
+
+          case 'alert':         // user informational event ie.: auto-pilot waypoint reached, Engine Started/stopped, ect.
             if (alarm.notification['method'].includes('sound')) { aSev = 1; }
             if (alarm.notification['method'].includes('visual')) { vSev = 1; }
             break;
-          case 'alarm':
-          case 'emergency':
+
+          case 'warn':          // user attention needed ie.: auto-pilot detected Wind Shift (go check if it's all fine), bilge pump activated (check if you have an issue).
             if (alarm.notification['method'].includes('sound')) { aSev = 2; }
             if (alarm.notification['method'].includes('visual')) { vSev = 2; }
+            break;
 
+          case 'alarm':         // a problem that requires immediate user attention ie.: auto-pilot can't stay on course, engine temp above specs.
+            if (alarm.notification['method'].includes('sound')) { aSev = 3; }
+            if (alarm.notification['method'].includes('visual')) { vSev = 3; }
+            break;
+
+          case 'emergency':     // safety threatening event ie.: MOB, collision eminent (AIS related), ran aground (water depth lower than keel draft)
+            if (alarm.notification['method'].includes('sound')) { aSev = 4; }
+            if (alarm.notification['method'].includes('visual')) { vSev = 4; }
+            break;
+
+          default: // we don;t know this one. Tell the user.
+            aSev = 0;
+            vSev = 0;
+            this.notificationsService.sendSnackbarNotification("Unknown Notification State received from SignalK", 0);
+            console.log("Unknown Notification State received from SignalK\n" + JSON.stringify(alarm));
         }
         audioSev = Math.max(audioSev, aSev);
-        visualSev = Math.max(visualSev, vSev)
+        visualSev = Math.max(visualSev, vSev);
       }
 
-      switch(audioSev) {
-        case 0:
-          this.warningSound.stop();
-          this.critSound.stop();
-          break;
-        case 1:
-          this.warningSound.play();
-          this.critSound.stop();
-          break;
-        case 2:
-          this.warningSound.stop();
-          this.critSound.play();
-
-      }
       switch(visualSev) {
         case 0:
           this.blinkWarn = false;
@@ -141,7 +146,9 @@ export class AlarmMenuComponent implements OnInit, OnDestroy {
           this.blinkWarn = false;
 
       }
-
+      if (!this.notificationConfig.sound.disableSound) {
+        this.playAlarm(1000 + audioSev);
+      }
     }
   }
 
@@ -181,6 +188,54 @@ export class AlarmMenuComponent implements OnInit, OnDestroy {
 
   pathIgnored(path: string) {
     return this.ignoredPaths.includes(path);
+  }
+
+  /**
+   * play audio notification sound
+   * @param trackId track to play
+   */
+  playAlarm(trackId: number) {
+    if (this.activeAlarmSoundtrack == trackId) {   // same track, do nothing
+      return;
+    }
+
+    if (trackId == 1000) {   // Stop track
+      this.howlPlayer.stop();
+      return;
+    }
+
+    this.howlPlayer.stop();
+    this.howlPlayer = this.getPlayer(trackId);
+    this.howlPlayer.play();
+  }
+
+  /**
+   * Load player with a specific track in loop mode.
+   * @param track track ID to play. See const for definition
+   */
+  getPlayer(track: number): Howl {
+    this.activeAlarmSoundtrack = track;
+    let player = new Howl({
+        src: ['assets/' + alarmTrack[track] + '.mp3'],
+        autoUnlock: true,
+        autoSuspend: false,
+        autoplay: false,
+        preload: true,
+        loop: true,
+        onend: function() {
+          // console.log('Finished!');
+        },
+        onloaderror: function() {
+          console.log("player onload error");
+        },
+        onplayerror: function() {
+          console.log("player locked");
+          this.howlPlayer.once('unlock', function() {
+            this.howlPlayer.play();
+          });
+        }
+      });
+    return player;
   }
 
   /**
