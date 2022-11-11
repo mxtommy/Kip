5c8cd11b1b9849d2a37e88f4134f49c94cd46dd1
godind <godind@hotmail.com>
2022-11-10 23:56:37 -0500

    test fix

--- src/app/widget-login/login.component.spec.ts
+++ src/app/widget-login/login.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { WidgetLoginComponent } from './widget-login.component';
 
-describe('LoginComponent', () => {
+describe('WidgetLoginComponent', () => {
   let component: WidgetLoginComponent;
   let fixture: ComponentFixture<WidgetLoginComponent>;
 
