ab3de2dae41710374537410c5ba04b8761998d60
godind <godind@hotmail.com>
2022-11-25 16:23:33 -0500

    cleanup

--- src/app/app-interfaces.ts
+++ src/app/app-interfaces.ts
@@ -1,4 +1,3 @@
-import { state } from '@angular/animations';
 /*********************************************************************************
  * This file contains App (Kip) internal data interfaces.
  *
