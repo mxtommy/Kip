0b1ca9ece870f170c1fa8aca7a690797b42c4e24
Roberto Beccaceci <roberto@beccaceci.it>
2022-12-01 18:26:56 +0100

    On reply to the issue #85
    Added ability to change the text of the "Blank" widget to a custom one.
    The rendering has not been deliberately changed to maintain compatibility with the existing layout when using with default config.
    I propose to add auto sizing here as well
    
    I used the displayName configuration parameter to not add new parameters and keep file modifications to a minimum

--- src/app/widget-blank/widget-blank.component.ts
+++ src/app/widget-blank/widget-blank.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit, Input } from '@angular/core';
+import {ModalWidgetComponent} from '../modal-widget/modal-widget.component';
+import {MatDialog} from '@angular/material/dialog';
+import { WidgetManagerService, IWidget, IWidgetConfig } from '../widget-manager.service';
+
+const defaultConfig: IWidgetConfig = {
+  displayName: 'Blank'
+};
 
 @Component({
   selector: 'app-widget-blank',
@@ -9,9 +16,42 @@ export class WidgetBlankComponent implements OnInit {
   @Input('widgetUUID') widgetUUID: string;
   @Input('unlockStatus') unlockStatus: boolean;
 
-  constructor() { }
+  activeWidget: IWidget;
+  config: IWidgetConfig;
+
+  constructor(
+    public dialog: MatDialog,
+    private WidgetManagerService: WidgetManagerService
+  ) { }
 
   ngOnInit() {
+    this.activeWidget = this.WidgetManagerService.getWidget(this.widgetUUID);
+    if (this.activeWidget.config === null) {
+      // no data, let's set some!
+      this.WidgetManagerService.updateWidgetConfig(this.widgetUUID, defaultConfig);
+      this.config = defaultConfig; // load default config.
+    } else {
+      this.config = this.activeWidget.config;
+    }
+  }
+
+
+  openWidgetSettings(content) {
+
+    const dialogRef = this.dialog.open(ModalWidgetComponent, {
+      width: '80%',
+      data: this.config
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      // save new settings
+      if (result) {
+        console.log(result);
+        this.config = result;
+        this.WidgetManagerService.updateWidgetConfig(this.widgetUUID, this.config);
+      }
+
+    });
   }
 
 }
