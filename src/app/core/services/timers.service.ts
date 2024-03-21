5ef7bdeaa9851d97db3d7d2bb392713e76574218
godind <godind@hotmail.com>
2024-03-21 18:27:01 -0400

    Changing theme causes Race Timer widget to lose track of is start/pause state Fixes #328

--- src/app/core/services/timers.service.ts
+++ src/app/core/services/timers.service.ts
@@ -1,11 +1,11 @@
-import { Injectable } from '@angular/core';
+import { Injectable, ɵisComponentDefPendingResolution } from '@angular/core';
 
 import { BehaviorSubject, Observable } from 'rxjs';
 
 
 interface IKipTimer {
   currentValue: BehaviorSubject<number>;
-  timeoutID: ReturnType<typeof setInterval>|null;
+  timeoutID: ReturnType<typeof setInterval> | null;
   intervalMS: number;
 }
 
@@ -20,11 +20,23 @@ export class TimersService {
 
   kipTimers: IKipTimers = {};
 
-  constructor() { 
-    
+  constructor() {
+
   }
 
-  createTimer(timerName: string, count: number, timerInterval: number): Observable<number> {
+  /**
+   * Create service timer. The timer notification schedules is based on the timerInterval value and
+   * the number of count to apply. Adjust the interval according to the use case and reduce
+   * unnecessary app events. A timerInterval of 1000 ms with count of 60 is 60 * 1000ms =
+   * 1 minutes and would generate a Subject event and Observer processing every second.
+   *
+   * @param {string} timerName Used as the ID id the timer
+   * @param {number} count Number of steps to execute. This number is a factor of timerInterval. Use negative values to countdown
+   * @param {number} timerInterval Count interval in milliseconds
+   * @return {*}  {Observable<number>} Timer count value as a number
+   * @memberof TimersService
+   */
+  public createTimer(timerName: string, count: number, timerInterval: number): Observable<number> {
     // return if exists
     if (timerName in this.kipTimers) {
       return this.kipTimers[timerName].currentValue.asObservable();
@@ -39,18 +51,18 @@ export class TimersService {
   }
 
 
-  startTimer(timerName: string) {
-    if (!this.kipTimers.hasOwnProperty(timerName)) { return; }
-    
-    if (this.kipTimers[timerName].timeoutID !== null) { return ; } // already running
 
-    this.kipTimers[timerName].timeoutID = setInterval(()=>{
+  public startTimer(timerName: string) {
+    if (!this.kipTimers.hasOwnProperty(timerName)) { return; }
+
+    if (this.kipTimers[timerName].timeoutID !== null) { return } // already running
+
+    this.kipTimers[timerName].timeoutID = setInterval(() => {
       this.kipTimers[timerName].currentValue.next(this.kipTimers[timerName].currentValue.value + 1)
     }, this.kipTimers[timerName].intervalMS);
-    
   }
 
-  stopTimer(timerName: string) {
+  public stopTimer(timerName: string) {
     if (!this.kipTimers.hasOwnProperty(timerName)) { return; }
     if (this.kipTimers[timerName].timeoutID === null) { return; } // already Stopped
     clearInterval(this.kipTimers[timerName].timeoutID);
@@ -58,16 +70,27 @@ export class TimersService {
   }
 
 
-  setTimer(timerName: string, timerValue: number) {
+  public setTimer(timerName: string, timerValue: number) {
     if (!this.kipTimers.hasOwnProperty(timerName)) { return; }
     this.kipTimers[timerName].currentValue.next(timerValue);
   }
 
 
-  deleteTimer(timerName: string) {
+  public deleteTimer(timerName: string) {
     if (!this.kipTimers.hasOwnProperty(timerName)) { return; }
     this.stopTimer(timerName);
     delete this.kipTimers[timerName];
   }
+
+
+  public isRunning(timerName: string) : boolean {
+    let running: boolean = false;
+    if (timerName in this.kipTimers) {
+      running = this.kipTimers[timerName].timeoutID === null ? false : true;
+    };
+
+    return running;
+  }
+
 
 }
