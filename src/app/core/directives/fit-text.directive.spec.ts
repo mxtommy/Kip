9e663956e0fb80ad12c94543684c4a69a5b417ea
godind <godind@hotmail.com>
2024-03-31 19:43:48 -0400

    fit-text removal

--- src/app/core/directives/fit-text.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FitTextDirective } from './fit-text.directive';
-
-describe('FitTextDirective', () => {
-  it('should create an instance', () => {
-    const directive = new FitTextDirective(el);
-    expect(directive).toBeTruthy();
-  });
-});
