725addaae0c781878e5276fa629f4258e70f9380
godind <godind@hotmail.com>
2024-05-08 00:05:01 -0400

    create

---
+++ src/app/widgets/widget-gauge-ng-compass/widget-gauge-ng-compass.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-widget-gauge-ng-compass',
+  standalone: true,
+  imports: [],
+  templateUrl: './widget-gauge-ng-compass.component.html',
+  styleUrl: './widget-gauge-ng-compass.component.scss'
+})
+export class WidgetGaugeNgCompassComponent {
+
+}
