20498b4e739cbdd5a8667d1b08611766e5fd6f46
godind <godind@hotmail.com>
2024-07-10 20:14:28 -0400

    fix radial capacity gauge

--- src/app/widgets/widget-gauge-ng-radial/widget-gauge-ng-radial.component.ts
+++ src/app/widgets/widget-gauge-ng-radial/widget-gauge-ng-radial.component.ts
@@ -74,8 +74,8 @@ export class WidgetGaugeNgRadialComponent extends BaseWidgetComponent implements
         type: "linear"
       },
       gauge: {
-        type: 'ngRadial', // capacity, measuring, marineCompass, baseplateCompass
-        subType: 'measuring', // capacity, measuring, marineCompass, baseplateCompass
+        type: 'ngRadial',
+        subType: 'measuring', // capacity, measuring
         enableTicks: true,
         compassUseNumbers: false
       },
@@ -134,14 +134,14 @@ export class WidgetGaugeNgRadialComponent extends BaseWidgetComponent implements
           default:
             option.colorBorderOuter = this.theme.cardColor;
             option.colorBorderMiddle = this.theme.cardColor;
-            option.colorBarProgress = this.getColors(this.widgetProperties.config.color).color;
+            option.colorBarProgress = this.widgetProperties.config.gauge.subType == 'measuring' ? this.getColors(this.widgetProperties.config.color).color : this.getColors(this.widgetProperties.config.color).dim;
         }
         this.radialGauge.update(option);
       }
     });
 
     this.metaSub = this.zones$.subscribe(zones => {
-      if (zones && zones.length > 0 && this.widgetProperties.config.gauge.subType == "measuring") {
+      if (zones && zones.length > 0) {
         this.setHighlights(zones);
       }
     });
@@ -160,20 +160,20 @@ export class WidgetGaugeNgRadialComponent extends BaseWidgetComponent implements
     this.gaugeOptions.title = this.widgetProperties.config.displayName ? this.widgetProperties.config.displayName : "";
     this.gaugeOptions.highlights = [];
 
-    this.gaugeOptions.fontTitle="Roboto";
-    this.gaugeOptions.fontTitleWeight="bold";
-    this.gaugeOptions.fontUnits="Roboto";
-    this.gaugeOptions.fontUnitsSize=25;
-    this.gaugeOptions.fontUnitsWeight="normal";
-    this.gaugeOptions.barStrokeWidth=0;
-    this.gaugeOptions.barShadow=0;
-    this.gaugeOptions.colorBarStroke="";
-    this.gaugeOptions.fontValue="Roboto";
-    this.gaugeOptions.fontValueWeight="bold";
-    this.gaugeOptions.valueTextShadow=false;
-    this.gaugeOptions.colorValueBoxShadow="";
-    this.gaugeOptions.fontNumbers="Roboto";
-    this.gaugeOptions.fontNumbersWeight="bold";
+    this.gaugeOptions.fontTitle = "Roboto";
+    this.gaugeOptions.fontTitleWeight = "bold";
+    this.gaugeOptions.fontUnits = "Roboto";
+    this.gaugeOptions.fontUnitsSize = 25;
+    this.gaugeOptions.fontUnitsWeight = "normal";
+    this.gaugeOptions.barStrokeWidth = 0;
+    this.gaugeOptions.barShadow = 0;
+    this.gaugeOptions.colorBarStroke = "";
+    this.gaugeOptions.fontValue = "Roboto";
+    this.gaugeOptions.fontValueWeight = "bold";
+    this.gaugeOptions.valueTextShadow = false;
+    this.gaugeOptions.colorValueBoxShadow = "";
+    this.gaugeOptions.fontNumbers = "Roboto";
+    this.gaugeOptions.fontNumbersWeight = "bold";
 
     this.gaugeOptions.valueInt = this.widgetProperties.config.numInt;
     this.gaugeOptions.valueDec = this.widgetProperties.config.numDecimal;
@@ -199,8 +199,8 @@ export class WidgetGaugeNgRadialComponent extends BaseWidgetComponent implements
     this.gaugeOptions.colorNeedle = this.getColors(this.widgetProperties.config.color).dim;
     this.gaugeOptions.colorNeedleEnd = this.getColors(this.widgetProperties.config.color).dim;
     // Labels
-    this.gaugeOptions.colorTitle = this.theme.whiteDim;//this.getColors(this.widgetProperties.config.color).dim;
-    this.gaugeOptions.colorUnits = this.theme.whiteDim;//this.getColors(this.widgetProperties.config.color).dim;
+    this.gaugeOptions.colorTitle = this.theme.whiteDim;
+    this.gaugeOptions.colorUnits = this.theme.whiteDim;
     this.gaugeOptions.colorValueText = this.getColors(this.widgetProperties.config.color).color;
     // Ticks
     this.colorStrokeTicks = this.getColors(this.widgetProperties.config.color).dim; // missing property in gaugeOptions
@@ -248,41 +248,47 @@ export class WidgetGaugeNgRadialComponent extends BaseWidgetComponent implements
     this.gaugeOptions.minValue = this.widgetProperties.config.displayScale.lower;
     this.gaugeOptions.maxValue = this.widgetProperties.config.displayScale.upper;
     this.gaugeOptions.units = this.widgetProperties.config.paths['gaugePath'].convertUnitTo;
-    this.gaugeOptions.colorMajorTicks = this.gaugeOptions.colorPlate; // bug with MajorTicks; always drawing first tick and using color="" does not work
-    this.gaugeOptions.colorNumbers = this.gaugeOptions.colorMinorTicks = "";
-    this.gaugeOptions.fontTitleSize = 60;
+    this.gaugeOptions.fontTitleSize = 40;
     this.gaugeOptions.barProgress = true;
-    this.gaugeOptions.barWidth = 15;
+    this.gaugeOptions.barWidth = 20;
+
+    this.gaugeOptions.colorBarProgress = this.getColors(this.widgetProperties.config.color).dim;
 
     this.gaugeOptions.valueBox = true;
-    this.gaugeOptions.fontValueSize = 110;
-    this.gaugeOptions.valueBoxWidth = 100;
-    this.gaugeOptions.valueBoxBorderRadius = 0;
+    this.gaugeOptions.fontValueSize = 60;
+    this.gaugeOptions.valueBoxWidth = 10;
+    this.gaugeOptions.valueBoxBorderRadius = 5;
     this.gaugeOptions.valueBoxStroke = 0;
-    this.gaugeOptions.colorValueBoxBackground = "";
+    this.gaugeOptions.colorValueBoxBackground = '';
+    this.gaugeOptions.colorValueBoxRect = '';
+    this.gaugeOptions.colorValueBoxRectEnd = '';
 
     this.gaugeOptions.ticksAngle = 360;
     this.gaugeOptions.startAngle = 180;
+    this.gaugeOptions.majorTicks = 0;
     this.gaugeOptions.exactTicks = true;
     this.gaugeOptions.strokeTicks = false;
     this.gaugeOptions.minorTicks = 0;
     this.gaugeOptions.numbersMargin = 0;
     this.gaugeOptions.fontNumbersSize = 0;
 
+    this.gaugeOptions.colorMajorTicks = this.gaugeOptions.colorPlate; // canvas gauge bug with MajorTicks; always drawing first tick and using color="" does not work
+    this.gaugeOptions.colorNumbers = this.gaugeOptions.colorMinorTicks = "";
+
     this.gaugeOptions.needle = true;
     this.gaugeOptions.needleType = this.LINE;
     this.gaugeOptions.needleWidth = 2;
     this.gaugeOptions.needleShadow = false;
-    this.gaugeOptions.needleStart = 80;
+    this.gaugeOptions.needleStart = 75;
     this.gaugeOptions.needleEnd = 95;
     this.gaugeOptions.needleCircleSize = 1;
     this.gaugeOptions.needleCircleInner = false;
     this.gaugeOptions.needleCircleOuter = false;
 
     this.gaugeOptions.borders = true;
-    this.gaugeOptions.borderOuterWidth = 0;
-    this.gaugeOptions.borderMiddleWidth = 2;
-    this.gaugeOptions.borderInnerWidth = 2;
+    this.gaugeOptions.borderOuterWidth = 2;
+    this.gaugeOptions.borderMiddleWidth = 1;
+    this.gaugeOptions.borderInnerWidth = 0;
     this.gaugeOptions.borderShadowWidth = 0;
 
     this.gaugeOptions.animationTarget = this.ANIMATION_TARGET_NEEDLE;
