45cb4754315b3237f41bccf755531a2edfcba1a2
Illidian4368 <illidian4368@gmail.com>
2025-02-02 01:34:20 +0100

    Add new Angular library
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/Illidian4368/Kip?shareId=XXXX-XXXX-XXXX-XXXX).

---
+++ projects/new-library/src/lib/sample.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SampleService {
+  constructor() { }
+
+  getSampleData(): string {
+    return 'This is sample data from the SampleService.';
+  }
+}
