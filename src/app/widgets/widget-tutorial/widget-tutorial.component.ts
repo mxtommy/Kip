66060106784634dadc427002929bf0ba5d0338d0
David G <34038713+godind@users.noreply.github.com>
2025-03-21 17:28:20 -0400

    Adjust Tutorial Widget (#518)

--- src/app/widgets/widget-tutorial/widget-tutorial.component.ts
+++ src/app/widgets/widget-tutorial/widget-tutorial.component.ts
@@ -2,7 +2,6 @@ import { Component, inject, OnDestroy } from '@angular/core';
 import { BaseWidgetComponent } from '../../core/utils/base-widget.component';
 import { WidgetHostComponent } from '../../core/components/widget-host/widget-host.component';
 import { IWidgetSvcConfig } from '../../core/interfaces/widgets-interface';
-import { RouterLink } from '@angular/router';
 import { MatButton } from '@angular/material/button';
 import { AppSettingsService } from '../../core/services/app-settings.service';
 import { DashboardService } from '../../core/services/dashboard.service';
@@ -10,8 +9,9 @@ import { DashboardService } from '../../core/services/dashboard.service';
 @Component({
     selector: 'widget-tutorial',
     templateUrl: './widget-tutorial.component.html',
+    styleUrls: ['./widget-tutorial.component.scss'],
     standalone: true,
-    imports: [ WidgetHostComponent, MatButton, RouterLink]
+    imports: [ WidgetHostComponent, MatButton]
 })
 export class WidgetTutorialComponent extends BaseWidgetComponent implements OnDestroy {
   protected dashboard = inject(DashboardService);
