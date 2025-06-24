3c80cfd35aa8c8852e5ab0586544014be42d21f3
Greg Wilkins <gregw@webtide.com>
2025-06-24 17:13:41 +1000

    still confused

--- src/app/widgets/widget-racer-timer/widget-racer-timer.component.ts
+++ src/app/widgets/widget-racer-timer/widget-racer-timer.component.ts
@@ -1,99 +1,355 @@
-import { Component, OnInit, OnDestroy, viewChild, inject, effect } from '@angular/core';
-
-import { SignalkRequestsService, skRequest } from '../../core/services/signalk-requests.service';
-import { BaseWidgetComponent } from '../../core/utils/base-widget.component';
-import { WidgetHostComponent } from '../../core/components/widget-host/widget-host.component';
-import { IWidgetSvcConfig } from '../../core/interfaces/widgets-interface';
-import { NgIf } from '@angular/common';
-import {FormsModule} from '@angular/forms';
-import { MatButton } from '@angular/material/button';
+import {Component, effect, ElementRef, inject, OnDestroy, OnInit, viewChild} from '@angular/core';
+import {Subscription} from 'rxjs';
+import {WidgetHostComponent} from '../../core/components/widget-host/widget-host.component';
+import {IWidgetSvcConfig} from '../../core/interfaces/widgets-interface';
+import {NgxResizeObserverModule} from 'ngx-resize-observer';
+import {BaseWidgetComponent} from '../../core/utils/base-widget.component';
+import {TimersService} from '../../core/services/timers.service';
+import {DashboardService} from '../../core/services/dashboard.service';
+import {States} from '../../core/interfaces/signalk-interfaces';
+import {NgIf} from '@angular/common';
+import {MatButton} from '@angular/material/button';
+import {CanvasService} from '../../core/services/canvas.service';
+import {result} from 'lodash-es';
 
 @Component({
-    selector: 'widget-racer-timer',
+    selector: 'widget-racertimer',
     templateUrl: './widget-racer-timer.component.html',
     styleUrls: ['./widget-racer-timer.component.scss'],
     standalone: true,
-  imports: [WidgetHostComponent, MatButton, NgIf, FormsModule]
+    imports: [ WidgetHostComponent, NgxResizeObserverModule, MatButton, NgIf ]
 })
-
 export class WidgetRacerTimerComponent extends BaseWidgetComponent implements OnInit, OnDestroy {
+  private TimersService = inject(TimersService);
+  private canvas = inject(CanvasService);
 
-  signalkRequestsService = inject(SignalkRequestsService);
+  private DashboardService = inject(DashboardService);
+  readonly canvasTimer = viewChild<ElementRef<HTMLCanvasElement>>('canvasTimer');
+  protected dataValue: number = null;
+  private zoneState: string = null;
 
-  readonly startTimerBtn = viewChild<MatButton>('startTimerBtn');
+  // length (in characters) of value text to be displayed. if changed from last time, need to recalculate font size...
+  private currentValueLength = 0;
+  private valueFontSize = 1;
+  private flashOn = false;
+  private flashInterval = null;
+  public timerRunning = false;
+  public showAdjustStartLine = true;
+  public showAdjustTimer = false;
+  readonly timeName: string = 'racer';
+  private warnColor: string = null;
+  private warmContrast: string = null;
+  private textColor: string = null;
 
-  showTopTTS = true;
-  showBottomDTS = true;
-  timeToStart = '0:05:00';
-  distanceToLine = '-';
-  startTime = 'HH:MM:SS';
+  timerSub: Subscription = null;
+
+  private canvasCtx: CanvasRenderingContext2D = null;
 
   constructor() {
-    console.log(`WidgetRacerTimerComponent constructor`);
     super();
+
     this.defaultConfig = {
-      displayName: 'N2k Autopilot',
-      filterSelfPaths: true,
-      paths: {
-        'timeToStart': {
-          description: 'Period in seconds to the Start time',
-          path: 'self.navigation.racing.timeToStart',
-          source: 'default',
-          pathType: 'number',
-          convertUnitTo: 's',
-          isPathConfigurable: true,
-          showPathSkUnitsFilter: false,
-          pathSkUnitsFilter: 's',
-          sampleTime: 500
-        },
-      }
+      timerLength: 300,
+      nextDashboard: 1,
+      color: 'contrast',
     };
+
+    effect(() => {
+      if (this.theme()) {
+        this.getColors(this.widgetProperties.config.color);
+        this.updateCanvas();
+      }
+    });
   }
 
   ngOnInit(): void {
-    console.log('WidgetRacerTimerComponent ngOnInit');
     this.validateConfig();
+    this.subscribeTimer();
+    this.startWidget();
   }
 
   protected startWidget(): void {
-    console.log('WidgetRacerTimerComponent startWidget');
-  }
-
-  ngOnDestroy(): void {
-    this.destroyDataStreams();
-    // this.unsubscribeSKRequest();
-    console.log('WidgetRacerTimerComponent ngOnDestroy');
+    this.getColors(this.widgetProperties.config.color);
+    this.canvasCtx = this.canvasTimer().nativeElement.getContext('2d');
   }
 
   protected updateConfig(config: IWidgetSvcConfig): void {
-    console.log('WidgetRacerTimerComponent updateConfig', JSON.stringify(config));
     this.widgetProperties.config = config;
+    this.startWidget();
+    this.updateCanvas();
   }
 
-  adjustLineEnd(end: string, delta: number, rotate: number) {
+  onResized(event: ResizeObserverEntry) {
+    if (event.contentRect.height < 50) { return; }
+    if (event.contentRect.width < 50) { return; }
+    if ((this.canvasTimer().nativeElement.width !== Math.floor(event.contentRect.width)) ||
+      (this.canvasTimer().nativeElement.height !== Math.floor(event.contentRect.height))) {
+      this.canvasTimer().nativeElement.width = Math.floor(event.contentRect.width);
+      this.canvasTimer().nativeElement.height = Math.floor(event.contentRect.height / 2);
+      this.currentValueLength = 0; // will force resetting the font size
+      this.updateCanvas();
+    }
   }
 
-  toggleTopPane() {
-    this.showTopTTS = !this.showTopTTS;
-    console.log('WidgetRacerTimerComponent toggleTopPane');
+  private subscribeTimer() {
+    this.timerRunning = this.TimersService.isRunning(this.timeName);
+    const length = this.widgetProperties.config.timerLength;
+    this.timerSub = this.TimersService.createTimer(this.timeName, -length, 1000).subscribe(
+      newValue => {
+        this.dataValue = newValue;
+
+        if (newValue > 0) {
+          this.zoneState = States.Normal;
+        } else if (newValue > -10) {
+          this.zoneState = States.Alarm;
+        } else if (newValue > -30) {
+          this.zoneState = States.Warn;
+        } else {
+          this.zoneState = States.Normal;
+        }
+
+        // start flashing if alarm
+        if (this.zoneState === States.Alarm && !this.flashInterval) {
+          this.flashInterval = setInterval(() => {
+            this.flashOn = !this.flashOn;
+            this.updateCanvas();
+          }, 500); // used to flash stuff in alarm
+        } else if (this.zoneState !== States.Alarm) {
+          // stop alarming if not in alarm state
+          clearInterval(this.flashInterval);
+        }
+        this.updateCanvas();
+
+        // Set the time on the server and check if OCS
+        fetch(`/plugins/signalk-racer/startline/timeToStart`, {
+          method: 'PUT', body: JSON.stringify({ value: newValue })
+        }).then(response => {
+          if (response.ok) {
+            response.text().then(text => {
+              const json = JSON.parse(text);
+              if (json.distanceToStart && typeof json.distanceToStart === 'number' ) {
+                if (json.distanceToStart < 0) {
+                  // TODO highlight the onCourseSide display element
+                }
+                if (json.timeToStart && typeof json.timeToStart === 'number' && json.timeToStart === 0 &&
+                  this.widgetProperties.config.nextDashboard >= 0) {
+                 this.DashboardService.navigateTo(this.widgetProperties.config.nextDashboard);
+                }
+              }
+            });
+          }
+        }).catch(error => {
+          console.error(`Error setting timeToStart:`, error);
+        });
+      }
+    );
   }
 
-  toggleBottomPane() {
-    this.showBottomDTS = !this.showBottomDTS;
-    console.log('WidgetRacerTimerComponent toggleBottomPane');
+  public startTimer() {
+    this.TimersService.startTimer(this.timeName);
+    this.timerRunning = true;
   }
 
-  setLineEnd(end: string) {
+  public resetTimer() {
+    this.unsubscribeTimer();
+    this.TimersService.deleteTimer(this.timeName);
+    this.timerRunning = false;
+    this.subscribeTimer();
   }
 
-  adjustStartTime(deltaSeconds: number) {
+  public pauseTimer() {
+    this.TimersService.stopTimer(this.timeName);
+    this.timerRunning = false;
   }
 
-  sendTimerCommand(command: string) {
+  public roundToMin() {
+    let v = this.dataValue;
+    if (this.dataValue < 0) {
+      v = v * -1; // always positive
+    }
+    const seconds = v % 60;
 
+    if (this.dataValue > 0) {
+      if (seconds > 30) {
+        this.TimersService.setTimer(this.timeName, this.dataValue + (60 - seconds));
+      } else {
+        this.TimersService.setTimer(this.timeName, this.dataValue - seconds);
+      }
+    } else {
+      if (seconds > 30) {
+        this.TimersService.setTimer(this.timeName, this.dataValue - (60 - seconds));
+      } else {
+        this.TimersService.setTimer(this.timeName, this.dataValue + seconds);
+      }
+    }
+  }
+  private setLineEnd(end: 'port' | 'stb') {
+    const url = `/plugins/signalk-racer/startline/${end}`;
+    fetch(url, { method: 'PUT' })
+      .then(async response => {
+        if (!response.ok) {
+          const err = await response.text();
+          console.error(`Failed to set ${end} end: ${response.status} - ${err}`);
+        } else {
+          console.log(`✅ Set start line ${end} end successfully.`);
+        }
+      })
+      .catch(error => {
+        console.error(`Error setting ${end} end:`, error);
+        alert(`Error setting ${end} end: ${error.message}`);
+      });
   }
 
-  setStartTimeAt(startTime: string) {
+  setLinePort() {
+    this.setLineEnd('port');
+  }
 
+  setLineStb() {
+    this.setLineEnd('stb');
+  }
+
+  addOneMin() {
+      this.TimersService.setTimer(this.timeName, this.dataValue + 60);
+  }
+
+  remOneMin() {
+      this.TimersService.setTimer(this.timeName, this.dataValue - 60);
+  }
+
+  private getColors(color: string) {
+    switch (color) {
+      case 'contrast':
+        this.textColor = this.theme().contrast;
+        this.warnColor = this.theme().zoneAlarm;
+        this.warmContrast = this.theme().zoneAlarm;
+        break;
+      case 'blue':
+        this.textColor = this.theme().blue;
+        this.warnColor = this.theme().zoneAlarm;
+        this.warmContrast = this.theme().zoneAlarm;
+        break;
+      case 'green':
+        this.textColor = this.theme().green;
+        this.warnColor = this.theme().zoneAlarm;
+        this.warmContrast = this.theme().zoneAlarm;
+        break;
+      case 'pink':
+        this.textColor = this.theme().pink;
+        this.warnColor = this.theme().zoneAlarm;
+        this.warmContrast = this.theme().zoneAlarm;
+        break;
+      case 'orange':
+        this.textColor = this.theme().orange;
+        this.warnColor = this.theme().zoneAlarm;
+        this.warmContrast = this.theme().zoneAlarm;
+        break;
+      case 'purple':
+        this.textColor = this.theme().purple;
+        this.warnColor = this.theme().zoneAlarm;
+        this.warmContrast = this.theme().zoneAlarm;
+        break;
+      case 'grey':
+        this.textColor = this.theme().grey;
+        this.warnColor = this.theme().zoneAlarm;
+        this.warmContrast = this.theme().zoneAlarm;
+        break;
+      case 'yellow':
+        this.textColor = this.theme().yellow;
+        this.warnColor = this.theme().zoneAlarm;
+        this.warmContrast = this.theme().zoneAlarm;
+        break;
+      default:
+        this.textColor = this.theme().contrast;
+        this.warnColor = this.theme().zoneAlarm;
+        this.warmContrast = this.theme().zoneAlarm;
+        break;
+    }
+  }
+
+  private unsubscribeTimer() {
+      this.timerSub?.unsubscribe();
+  }
+
+  ngOnDestroy() {
+    this.timerSub?.unsubscribe();
+    if (this.canvasCtx) {
+      this.canvas.clearCanvas(this.canvasCtx, this.canvasTimer().nativeElement.width, this.canvasTimer().nativeElement.height);
+    }
+    clearInterval(this.flashInterval);
+    this.destroyDataStreams();
+  }
+
+/* ******************************************************************************************* */
+  /*                                  Canvas                                                     */
+  /* ******************************************************************************************* */
+
+  updateCanvas() {
+    if (this.canvasCtx) {
+      this.canvas.clearCanvas(this.canvasCtx, this.canvasTimer().nativeElement.width, this.canvasTimer().nativeElement.height);
+      this.drawValue();
+    }
+  }
+
+  drawValue() {
+    const canvasTimer = this.canvasTimer().nativeElement;
+    const maxTextWidth = Math.floor(canvasTimer.width * 0.95);
+    const maxTextHeight = Math.floor(canvasTimer.height);
+    let valueText: string;
+
+    if (this.dataValue != null) {
+      const v = Math.abs(this.dataValue); // Always positive
+      const m = Math.floor(v / 60);
+      const s = Math.floor(v % 60);
+      valueText = `${m}:${('0' + s).slice(-2)}`;
+
+      if (this.dataValue < 0) {
+        valueText = `-${valueText}`;
+      }
+    } else {
+      valueText = '--';
+    }
+
+    // Check if the length of the string has changed
+    if (this.currentValueLength !== valueText.length) {
+      this.currentValueLength = valueText.length;
+      this.valueFontSize = this.canvas.calculateOptimalFontSize(
+        this.canvasCtx,
+        valueText,
+        maxTextWidth,
+        maxTextHeight,
+        'bold'
+      );
+    }
+
+    // Set the text color based on the zone state
+    switch (this.zoneState) {
+      case States.Alarm:
+        if (this.flashOn) {
+          this.canvasCtx.fillStyle = this.textColor;
+        } else {
+          this.canvas.drawRectangle(this.canvasCtx, 0, 0, canvasTimer.width, canvasTimer.height, this.warnColor);
+          this.canvasCtx.fillStyle = this.textColor;
+        }
+        break;
+      case States.Warn:
+        this.canvasCtx.fillStyle = this.warnColor;
+        break;
+      default:
+        this.canvasCtx.fillStyle = this.textColor;
+    }
+
+    // Draw the text
+    this.canvas.drawText(
+      this.canvasCtx,
+      valueText,
+      canvasTimer.width / 2,
+      canvasTimer.height / 2,
+      maxTextWidth,
+      maxTextHeight,
+      'bold',
+      this.canvasCtx.fillStyle,
+      'center',
+      'middle'
+    );
   }
 }
